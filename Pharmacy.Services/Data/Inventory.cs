b63a09c237f5839535613a294a4645807b15b099
Reem Ahmed <reemahmed62005@gmail.com>
2025-09-20 22:38:47 +0300

    added sale, sales details and purchase

--- Pharmacy.Services/Data/Inventory.cs
+++ Pharmacy.Services/Data/Inventory.cs
@@ -11,14 +11,17 @@ namespace Pharmacy.Services.Data
         public int id { get; set; }
         public int ItemId { get; set; }
         public int? UnitId { get; set; }
-        public decimal Quantity { get; set; }
+        public int? PurchaseId { get; set; }
+        public int Quantity { get; set; }
         public DateTime? ExpiryDate { get; set; }
         public DateTime? ProductionDate { get; set; }
-        public decimal CostPrice { get; set; }
-        public decimal SellingPrice { get; set; }
+        public double CostPrice { get; set; }
+        public double VAT { get; set; }
+        public int QuantityAvail { get; set; }
        
         // Navigation properties
         public Item? Item { get; set; }
         public Unit? Unit { get; set; }
+        public Purchase Purchase { get; set; }
     }
 }
---
+++ Pharmacy.Services/Data/Sales.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacy.Services.Data
+{
+    public class Sales
+    {
+        public int Id { get; set; }
+        public string? ClientName { get; set; }
+        public DateTime SalesDate { get; set; }
+
+    }
+}
